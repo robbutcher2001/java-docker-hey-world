3c590df8aa269e73571f81e549e21c4e8e86f6b1
Rob Butcher <rob.butcher2@baesystems.com>
2018-05-31 17:13:07 +0100

    Deploy to server

--- application/src/main/java/hello/app/DemoApplication.java
+++ application/src/main/java/hello/app/DemoApplication.java
@@ -5,23 +5,44 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+interface NumericTest {
+	int computeTest(int x, int y);
+}
 
 @SpringBootApplication(scanBasePackages = "hello")
 @RestController
 public class DemoApplication {
 
-    private final MyService myService;
+  private final MyService myService;
+  private final List<Integer> myList = new ArrayList<Integer>();
 
-    public DemoApplication(MyService myService) {
-        this.myService = myService;
+  public DemoApplication(MyService myService) {
+    this.myService = myService;
+  }
+
+  @GetMapping("/")
+  public String home() {
+    for(int i=0; i<10; i++) {
+      myList.add(i);
     }
 
-    @GetMapping("/")
-    public String home() {
-        return myService.message();
-    }
+    myList.forEach(new Consumer<Integer>() {
+      public void accept(Integer t) {
+        System.out.println("forEach anonymous class Value: " + t);
+      }
+    });
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+    NumericTest z = (int x, int y) -> x * y;
+    System.out.println(z.computeTest(4, 9));
+
+    return myService.message();
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 }
